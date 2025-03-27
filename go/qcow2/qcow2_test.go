3a7a67e3cc2d35d698b8a0dcaef1c52d5e92c173
Tim Windelschmidt <tim@monogon.tech>
2025-03-27 21:59:36 +0100

    go/qcow2: migrate to qemu-img in toolchain-bundle
    Change-Id: I65a542932233d120bd2015dacd0efe4a9935705e
    Reviewed-on: https://review.monogon.dev/c/monogon/+/4051
    Tested-by: Jenkins CI
    Reviewed-by: Jan Schär <jan@monogon.tech>

--- go/qcow2/qcow2_test.go
+++ go/qcow2/qcow2_test.go
@@ -9,7 +9,28 @@ import (
 	"os/exec"
 	"path/filepath"
 	"testing"
+
+	"github.com/bazelbuild/rules_go/go/runfiles"
 )
+
+var (
+	// These are filled by bazel at linking time with the canonical path of
+	// their corresponding file. Inside the init function we resolve it
+	// with the rules_go runfiles package to the real path.
+	xQEMUImgPath string
+)
+
+func init() {
+	var err error
+	for _, path := range []*string{
+		&xQEMUImgPath,
+	} {
+		*path, err = runfiles.Rlocation(*path)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
 
 // TestGenerate exercises the Generate function for a variety of image sizes.
 func TestGenerate(t *testing.T) {
@@ -29,7 +50,7 @@ func TestGenerate(t *testing.T) {
 				t.Fatalf("Close: %v", err)
 			}
 
-			cmd := exec.Command("qemu-img", "check", path)
+			cmd := exec.Command(xQEMUImgPath, "check", path)
 			if err := cmd.Run(); err != nil {
 				t.Fatalf("qemu-img check failed: %v", err)
 			}
